47990d9c4ee4b43c5710ac97f8b0f90a9ad0f67b
Nancy Singh <097nancysingh@gmail.com>
2026-02-22 23:10:36 +0530

    Reformat server.js for improved readability

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-// server.js
+
 require('dotenv').config(); // Loads secret variables from the .env file
 const express = require('express');
 const connectDB = require('./config/db'); // Imports the database connection
 
-// Initialize the Express application
+
 const app = express();
 
-// Connect to MongoDB
+
 connectDB();
 
 // Middleware: Allows our API to understand JSON data sent in requests
 app.use(express.json());
 
 
-// -------- ROUTES ARE HERE --------
+
 // User routes
 app.use('/api/users', require('./routes/userRoutes'));
 
@@ -22,15 +22,16 @@ app.use('/api/books', require('./routes/bookroutes'));
 // ---------------------------------
 
 
-// A simple test route just to say hello
+
 app.get('/', (req, res) => {
     res.send('Book Catalog API is running!');
 });
 
-// Define the port from our .env file, or use 5000 as a backup
+
 const PORT = process.env.PORT || 5000;
 
-// Start the server and listen for requests
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+
 });
